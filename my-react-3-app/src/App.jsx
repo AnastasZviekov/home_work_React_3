e6c00627a92502c35d82cc342283bf7bee89d4ce
AnastasZviekov <stas.zviekov@gmail.com>
2024-02-08 17:42:59 +0100

    add_final

---
+++ my-react-3-app/src/App.jsx
@@ -0,0 +1,16 @@
+import {useState} from 'react'
+import './App.css'
+import Menu from "./components/Menu/Menu.jsx";
+
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <Menu/>
+        </>
+    )
+}
+
+export default App
